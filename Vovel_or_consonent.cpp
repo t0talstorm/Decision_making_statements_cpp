07c4e7fc61fffcb1377b05a64d49279054959de1
Aditya Sinha <sinha.aditya.btech2024@sitpune.edu.in>
2025-07-16 23:50:14 -0400

    Experiment - 5B

---
+++ Vovel_or_consonent.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    char character;
+    cout << "Please Enter a single character : ";
+    cin >> character;
+    
+    char character1 = tolower(character);
+    
+    if(character1 == 'a' || character1 == 'e' ||character1 == 'i' ||character1 == 'o' ||character1 == 'u') {
+        cout << "The Character "<< character << " is a vovel";
+    }else cout<< "The character "<< character << " is Consonant";
+    return 0;
+}
+
+/*
+=========OUTPUT==========
+Please Enter a single character : H
+The character H is Consonant
+
+=== Code Execution Successful ===
+
+Please Enter a single character : E
+The Character E is a vovel
+
+=== Code Execution Successful ===
+*/
